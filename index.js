7400986bbd37cf0d52b42726ae7132c6e6281f7b
Pratik Hande <pratikhande30@gmail.com>
2019-12-06 14:44:53 +0530

    Changed Port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 const path = require('path')
 const http = require('http').Server(app); 
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -26,7 +26,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(5000, function(){
-  console.log('listening on *:5000');
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
